227507f2c37abd8a73bb91d06c67950a7237c740
nekoz <coriannicolas21@gmail.com>
2024-10-24 23:57:32 -0300

    arreglo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,9 @@ import { Link } from "react-router-dom";
 
 const routes = [
   {
-    path: "/", xmlns: "http://www.w3.org/2000/svg", fill: "currentColor", type: "bi bi-house", viewBox: "0 0 16 16",
-    d: "M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z",
+    path: "/", icon: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+    <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+  </svg>
   },
   { path: "/skills", label: "Skills" },
   { path: "/work", label: "Work" },
@@ -14,15 +15,11 @@ function Navbar() {
   return (
     <nav className="w-full h-16 sticky top-0 border-b border-white bg-[#1B2CC1] text-[#ABD2FA]">
       <ul className="flex justify-evenly items-center h-full">
-        {routes.map(({ path, label, xmlns, fill, type, viewBox, d }, index) => (
+        {routes.map(({ path, label,icon}, index) => (
           <li key={index}>
             <Link to={path} className="hover:text-white hover:underline">
               {label}
-              <svg xmlns={xmlns} width="16" height="16" fill={fill} type={type} viewBox={viewBox}>
-                <path d={d} />
-                <path />
-              </svg>
-
+              {icon}
             </Link>
           </li>
         ))}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import fondo from "../assets/astrofondo2.jpg"
-import Skills from './Skills'
+import { Link } from 'react-router-dom'
+import Contact from "./Contact";
 
 function Home() {
   return (
-    <div className=''>
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage: `url(${fondo})`,
-          backgroundSize: "100%",
-          backgroundRepeat: "no-repeat",
-        }}>
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Bienvenido!</h1>
-            <p className="mb-5">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto rem similique inventore, perspiciatis mollitia ex exercitationem excepturi odio corporis voluptates autem aliquid ut ducimus, repellat nesciunt laudantium magnam! Fuga, voluptas?
-            </p>
-            <button className="btn bg-[#1B2CC1] text-[#ABD2FA]">Get Started</button>
-          </div>
+    <div
+      className="hero h-full w-full"
+      style={{
+        backgroundImage: `url(${fondo})`,
+        backgroundSize: "100%",
+        backgroundRepeat: "no-repeat",
+      }}>
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Bienvenido!</h1>
+          <p className="mb-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto rem similique inventore, perspiciatis mollitia ex exercitationem excepturi odio corporis voluptates autem aliquid ut ducimus, repellat nesciunt laudantium magnam! Fuga, voluptas?
+          </p>
+          <Link to={"/contact"}>
+          <button className="btn bg-[#1B2CC1] text-[#ABD2FA]">Contact😳</button>
+          </Link>
         </div>
       </div>
 
